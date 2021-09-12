de1bcb2a86fcaf60e1d4ccc0b6a0066399330cae
RealistikDash <36131887+RealistikDash@users.noreply.github.com>
2021-09-12 15:13:12 +0100

    lru cache + web client capabilites

---
+++ conn/web_client.py
@@ -0,0 +1,22 @@
+# For now a simple wrapper around aiohttp.
+# Orjson is optional and can be replaced 1:1 by the default one. Only use
+# it when we have it.
+try: from orjson import loads as j_load
+except ImportError: from json import loads as j_load
+
+import aiohttp
+
+async def simple_get(url: str, args: dict = {}) -> str:
+    """Sends a simple `GET` request to `url` with GET args `args` and returns
+    the response body as a `str`."""
+
+    async with aiohttp.ClientSession() as s:
+        async with s.get(url, args=args) as res:
+            return await res.text()
+    
+async def simple_get_json(url: str, args: dict = {}) -> dict:
+    """Sends a simple `GET` request to `url` with GET args `args` and returns
+    the response body JSON as `dict`."""
+
+    res = await simple_get(url, args)
+    return j_load(res)
--- globs/caches.py
+++ globs/caches.py
@@ -2,12 +2,17 @@ from caches.clan import ClanCache
 from caches.bcrypt import BCryptCache
 from caches.priv import PrivilegeCache
 from caches.username import UsernameCache
+from caches.lru_cache import Cache
 from logger import info
 
+# Specialised Caches
 name = UsernameCache()
 priv = PrivilegeCache()
 clan = ClanCache()
 password = BCryptCache()
+
+# General Caches.
+beatmaps = Cache(cache_length= 120, cache_limit= 1000)
 
 #CACHE_INITS = (
 #    name.full_load,
