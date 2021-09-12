c0cb42318946ca56101f3a580538bafaa799c676
RealistikDash <36131887+RealistikDash@users.noreply.github.com>
2021-09-12 16:55:32 +0100

    beatmap classmethods for creating instances

--- objects/beatmap.py
+++ objects/beatmap.py
@@ -1,6 +1,13 @@
 from dataclasses import dataclass
+from libs.time import get_timestamp
+from typing import Optional
 from consts.modes import Mode
 from consts.statuses import Status
+from globs.caches import beatmaps
+from globs.conn import sql
+from logger import debug, error
+from conn.web_client import simple_get_json
+from config import conf
 
 @dataclass
 class Beatmap:
@@ -14,7 +21,7 @@ class Beatmap:
     ar: float = 0.0
     od: float = 0.0
     mode: Mode = Mode(0)
-    max_combo: int = 0
+    max_combo: Optional[int] = 0 # The osu API does not sometimes send this.
     hit_length: int = 0
     bpm: int = 0
     rating: int = 10
@@ -22,19 +29,211 @@ class Beatmap:
     passcount: int = 0
     last_update: int = 0
     status: Status = Status(0)
+    status_frozen: bool = False
     # Ripple schema difficulties
     difficulty_std: float = 0.0
     difficulty_taiko: float = 0.0
     difficulty_ctb: float = 0.0
     difficulty_mania: float = 0.0
 
+    ## PROPERTIES
     @property
     def difficulty(self) -> float:
         """Returns the star difficulty for the beatmap's main mode."""
 
-        return {
-            modes.STANDARD: self.difficulty_std,
-            modes.TAIKO: self.difficulty_taiko,
-            modes.CATCH: self.difficulty_ctb,
-            modes.MANIA: self.difficulty_mania
-        }[self.mode]
+        return self.__getattribute__(__diff_attribs[self.mode.value])
+    
+    ## CLASSMETHODS
+    @classmethod
+    async def from_oapi_v1(cls, md5: str) -> Optional['Beatmap']:
+        """Attempts to create an instance of `Beatmap` using data fetched from
+        the osu!api v1.
+
+        Note:
+            This is a slow function call due to creating a GET request to
+                an external API.
+            This function does not consider rate limits imposed by the osu API.
+                Use with care.
+        
+        Args:
+            md5 (str): The MD5 hash of the `.osu` beatmap file.
+
+        Returns:
+            Instance of `Beatmap` on successful fetch. Else `None`.
+        """
+
+        debug(f"Starting osu!api v1 fetch of beatmap {md5}")
+        try:
+            found_beatmaps = simple_get_json(
+                "https://old.ppy.sh/api/get_beatmaps", {
+                    "k": conf.osu_api_key,
+                    "h": md5
+                }
+            )
+        except Exception: return error("Failed to fetch map from the osu!api")
+        if not found_beatmaps:
+            return debug(f"Beatmap {md5} not found in the api.")
+        
+        map_json, = found_beatmaps
+        song_name = __create_full_name(
+            artist= map_json["artist"],
+            title= map_json["title"],
+            difficulty= map_json["version"]
+        )
+        max_combo = int(mc) if (mc := map_json["max_combo"]) else None
+
+        # We now create the object with the data.
+        # NOTE: All values sent by oapi v1 are strings.
+        bmap = Beatmap(
+            id= int(map_json["beatmap_id"]),
+            set_id= int(map_json["beatmapset_id"]),
+            md5= md5,
+            song_name= song_name,
+            ar= float(map_json["diff_approach"]),
+            od= float(map_json["diff_overall"]),
+            mode= Mode(int(map_json["mode"])), # You may not have to int it.
+            max_combo= max_combo,
+            hit_length= int(map_json["hit_length"]),
+            bpm= round(float(map_json["bpm"])),
+            last_update= get_timestamp()
+        )
+        # Set star diff for the main mode.
+        bmap.__setattr__(
+            __diff_attribs[bmap.mode.value],
+            round(float(map_json["difficultyrating"]), 2)
+        )
+
+        return bmap
+    
+    @classmethod
+    async def from_db(self, md5: str) -> Optional['Beatmap']:
+        """Fetches data from MySQL and creates an instance of `Beatmap` from it.
+        
+        Args:
+            md5 (str): The MD5 hash of the `.osu` beatmap file.
+
+        Returns:
+            Instance of `Beatmap` on successful fetch. Else `None`.
+        """
+
+        # This query is not very fun...
+        map_db = await sql.fetchone(
+            "SELECT beatmap_id, beatmapset_id, beatmap_md5, song_name, ar, od "
+            "mode, rating, difficulty_std, difficulty_taiko, difficulty_ctb, "
+            "difficulty_mania, max_combo, hit_length, bpm, playcount, passcount, "
+            "ranked, latest_update, ranked_status_freezed FROM beatmaps WHERE "
+            "beatmap_md5 = %s LIMIT 1", (md5,)
+        )
+
+        # Not found check.
+        if not map_db: return
+
+        return Beatmap(
+            id= map_db[0],
+            set_id= map_db[1],
+            md5= map_db[2],
+            song_name= map_db[3],
+            ar= map_db[4],
+            od= map_db[5],
+            mode= Mode(map_db[6]),
+            rating= map_db[7],
+            difficulty_std= map_db[8],
+            difficulty_taiko= map_db[9],
+            difficulty_ctb= map_db[10],
+            difficulty_mania= map_db[11],
+            max_combo= map_db[12],
+            hit_length= map_db[13],
+            bpm= map_db[14],
+            playcount= map_db[15],
+            passcount= map_db[16],
+            status= Status(map_db[17]),
+            last_update= int(map_db[18]),
+            status_frozen= not not map_db[19]
+        )
+    
+    @classmethod
+    async def from_cache(self, md5: str) -> Optional['Beatmap']:
+        """Tries to fetch an existing instance of `Beatmap` from the global
+        Beatmap cache.
+        
+        Args:
+            md5 (str): The MD5 hash of the `.osu` beatmap file.
+
+        Returns:
+            Instance of `Beatmap` on successful fetch. Else `None`.
+        """
+
+        beatmaps.get(md5)
+    
+    @classmethod
+    async def from_md5(self, md5: str)  -> Optional['Beatmap']:
+        """Attempts to create/fetch an instance of beatmap using multiple
+        sources ordered by speed. High level API.
+        
+        Note:
+            The order of sources is: Cache, MySQL, oapiv1.
+
+        Args:
+            md5 (str): The MD5 hash of the `.osu` beatmap file.
+
+        Returns:
+            Instance of `Beatmap` on successful fetch. Else `None`.
+        """
+
+        for fetch in __fetch_order:
+            res = await fetch(md5)
+            # We have our map.
+            if res:
+                # Check if we used a fetch that lets us cache.
+                if fetch in __cachable: res.cache()
+                if fetch in __insertable: await res.insert_db()
+                return res
+    
+    def cache(self) -> None:
+        """Caches the beatmap to the global beatmap cache.
+        
+        Note:
+            Raises `ValueError` if the beatmap id is equal to 0.
+        """
+
+        if not self.id: raise ValueError(
+            "Unloaded beatmaps (id = 0) may not be cached!"
+        )
+        beatmaps.cache(self.md5, self)
+    
+    async def insert_db(self, bypass_exist_check: bool = False) -> None:
+        """Inserts the beatmap into the MySQL database.
+        
+        Args:
+            bypass_exist_check (bool): If true, the check to make sure that
+                the beatmap is not already in the database will not be
+                performed.
+        """
+
+        ...
+
+__diff_attribs = {
+    Mode.STANDARD: "difficulty_std",
+    Mode.TAIKO:    "difficulty_taiko",
+    Mode.CATCH:    "difficulty_ctb",
+    Mode.MANIA:    "difficulty_mania"
+}
+
+__fetch_order = (
+    Beatmap.from_cache,
+    Beatmap.from_db,
+    Beatmap.from_oapi_v1,
+)
+__cachable = ( # Funcs that should be cached.
+    Beatmap.from_db,
+    Beatmap.from_oapi_v1,
+)
+
+__insertable = ( # Funcs that should be inserted into the db
+    Beatmap.from_oapi_v1,
+)
+
+def __create_full_name(artist: str, title: str, difficulty: str) -> str:
+    """Creates a full name out of song details for storage in the database."""
+
+    return f"{artist} - {title} [{difficulty}]"
