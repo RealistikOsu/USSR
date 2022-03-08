e37f40094d6523e79cf5e3a5a4c9ea10a57d2bb0
PANIGE <Nicolas.alary@gmx.fr>
2022-03-09 00:41:19 +0100

    Added folder requirements
    because automatically adding folders on running is cool

---
+++ helpers/folders.py
@@ -0,0 +1,27 @@
+"""Created a full dedicated file for 2 poor functions, i know."""
+from os.path import isdir
+from os import mkdir
+
+
+REQUIRED_FOLDERS = [
+    ".data/",
+    ".data/replays",
+    ".data/replays_relax",
+    ".data/replays_ap",
+    ".data/maps",
+]
+
+
+def verify_required_folders() -> bool:
+    """Verifies that all required folders for USSR to work properly"""
+    for folder in REQUIRED_FOLDERS:
+        if isdir(folder):
+            continue
+        else:
+            return False
+    return True
+
+
+def ensure_required_folders():
+    for folder in REQUIRED_FOLDERS:
+        mkdir(folder)
