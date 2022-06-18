f1be95a33d53eb7334bfdb30c7490508580a537f
tsunyoku <mbruhyo@gmail.com>
2022-06-19 01:03:13 +0200

    add score sub to router

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -4,7 +4,13 @@ from fastapi import APIRouter
 from fastapi import Response
 
 from . import leaderboards
+from . import score_sub
 
 router = APIRouter(default_response_class=Response)
 
 router.add_api_route("/web/osu-osz2-getscores.php", leaderboards.get_leaderboard)
+router.add_api_route(
+    "/web/osu-submit-modular-selector.php",
+    score_sub.submit_score,
+    methods=["POST"],
+)
