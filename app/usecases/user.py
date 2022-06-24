0470005242fd63bb966f755fd1cbf5bdc9e1db96
osu!Akatsuki Developer <support@akatsuki.pw>
2022-06-24 17:59:04 +0000

    fix autores privileges

--- app/usecases/user.py
+++ app/usecases/user.py
@@ -201,13 +201,12 @@ async def restrict_user(
     if user.privileges.is_restricted:
         return
 
-    user.privileges = user.privileges & ~Privileges.USER_PUBLIC
+    user.privileges = (user.privileges | Privileges.USER_NORMAL) & ~Privileges.USER_PUBLIC
     await app.state.services.database.execute(
-        "UPDATE users SET privileges = :new_priv, ban_datetime = :ban_time "
+        "UPDATE users SET privileges = :new_priv, ban_datetime = UNIX_TIMESTAMP() "
         "WHERE id = :id",
         {
             "new_priv": user.privileges,
-            "ban_time": int(time.time()),
             "id": user.id,
         },
     )
