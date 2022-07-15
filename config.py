bf1e62cc3fad2010a8f6d9983a21f19b7426ae44
RealistikDash <realistikdash@gmail.com>
2022-07-15 22:12:17 +0200

    refactor how the config deals with files

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
-from dataclasses import dataclass, field
-from json import dump, loads
+from dataclasses import dataclass
+from dataclasses import field
+from json import dump
+from json import loads
 from typing import Union
+from typing import Optional
+from typing import Any
+from typing import TypeVar
+import os
 
 from logger import debug
 from logger import info
@@ -9,119 +15,82 @@ __name__ = "ConfigModule"
 __author__ = "RealistikDash"
 __version__ = "v2.0.0"
 
-
-@dataclass
-class JsonFile:
-    file_name: str
-    file: dict = field(default_factory=dict)
-
-    def __post_init__(self) -> None:
-        """Reloads the file fully into memory."""
-
-        with open(self.file_name) as f:
-            self.file = loads(f.read())
-
-    def get_file(self) -> dict:
-        """Returns the loaded JSON file as a dict.
-        Returns:
-            Contents of the file.
-        """
-        return self.file
-
-    def write_file(self, new_content: Union[dict, list]) -> None:
-        """Writes `new_content` to the target file.
-        Args:
-            new_content (dict, list): The new content that should be placed
-                within the file.
-        """
-
-        with open(self.file_name, "w") as f:
-            dump(new_content, f, indent=4)
-
-        self.file = new_content
-
+T = TypeVar("T")
 
 class ConfigReader:
     """A parent class meant for the easy management, updating and the creation
     of a configuration `JSON` file."""
+    
+    _json: dict
+    updated_keys: Optional[list[str]] = None
+    updated: bool = False
 
-    def __init__(self):
-        """Sets placeholder variables."""
-
-        # Set to true if a new value was added to the config.
-        self.updated: bool = False
-        self.updated_keys: list = []
-
-        # An object around the configuration file.
-        self.json: JsonFile = JsonFile("config.json")
-
-    def __init_subclass__(cls, stop_on_update: bool = False):
+    def __init_subclass__(cls, config_path: str = "config.json") -> None:
         """Sets and reads the config child class."""
 
-        cls.__init__(cls)
+        super().__init_subclass__()
+        cls.read(cls, config_path)
 
-        # Now we read all of the annotated valiables.
+        # Read annotated variables into the class.
         for var_name, key_type in cls.__annotations__.items():
-
-            # We are checking for a possible default value (in case its a new field)
             default = getattr(cls, var_name, None)
-
-            # Read the key. json is lower case so we have to transform
-            key_val = cls.read_json(cls, var_name.lower(), default)
-
-            # Force it to be the sepcified type.
+            key_val = cls.get_config_attribute(cls, var_name.lower(), default)
             key_val = key_type(key_val)
-
-            # Set the attribute.
             setattr(cls, var_name, key_val)
 
         if cls.updated:
-            cls.on_finish_update(cls, cls.updated_keys)
+            cls.write(cls, config_path)
+            cls.on_finish_update(cls)
 
-    def on_finish_update(self, keys_updated: list):
+    def on_finish_update(self) -> None:
         """Called when the config has just been updated.
         This is meant to be overridden."""
 
         info(
             "The config has just been updated! Please edit according to your preferences!",
         )
-        debug("Keys added: " + ", ".join(keys_updated))
+        debug("Keys added: " + ", ".join(self.updated_keys)) # type: ignore
 
         raise SystemExit(0)
+    
+    def read(self, file_path: str) -> None:
+        """Reads the JSON file and sets it within the object."""
+        
+        if not os.path.exists(file_path):
+            # The updater will handle populating the schema
+            self._json = {}
+            return
 
-    def read_json(self, key: str, default=None):
-        """Reads a value directly from the json file and returns
-        if. If the value is not already in the JSON file, it adds
-        it and sets it as `default`.
-        Args:
-            key (str): The JSON key to fetch the value of.
-            default (any): The value for the key to be set to if the
-                value is not set.
-        Returns:
-            Value of the key.
-        """
+        # Read the file.
+        with open(file_path, "r") as f:
+            self._json = loads(f.read())
+    
+    def write(self, file_path: str) -> None:
+        """Writes the config into a json file."""
+        
+        # Contrict the config dict from annotations.
+        config_dict = {
+            var.lower(): getattr(self, var)
+            for var in self.__annotations__.keys()
+        }
+        
+        with open(file_path, "w") as f:
+            dump(config_dict, f, indent=4)
 
-        # Handle a case where the file is empty/not found.
-        if self.json.file is None:
-            # Set it to an empty dict so it can be handled with the thing below.
-            self.json.file = {}
-
-        # Check if the key is present. If not, set it.
-        if key not in tuple(self.json.file):
-            # Set it so we can check if the key was modified.
+    def get_config_attribute(self, key: str, default: Optional[T]=None) -> Optional[T]:
+        """Fetches the value of the given key from the config, returning the
+        default value."""
+        
+        value = self._json.get(key)
+        
+        if value is None:
+            if not self.updated_keys:
+                self.updated_keys = []
             self.updated = True
             self.updated_keys.append(key)
-            # Set the value in dict.
-            self.json.file[key] = default
-
-            # Write it to the file.
-            self.json.write_file(self.json.file)
-
-            # Return default
             return default
-
-        # It exists, just return it.
-        return self.json.file[key]
+        
+        return value
 
 
 # TODO: Notifications for config updates.
@@ -137,7 +106,7 @@ class Config(ConfigReader):
     DATA_DIR: str = ".data"
     DIRECT_URL: str = "https://catboy.best/api"
     API_KEYS_POOL: list = ["keys here"]
-    CUSTOM_CLIENTS: bool = False  # Allow custom clients on
+    CUSTOM_CLIENTS: bool = False
     SRV_URL: str = "https://ussr.pl"
     SRV_NAME: str = "RealistikOsu"
     SRV_VERIFIED_BADGE: int = 1005
