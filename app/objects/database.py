e22ef46ee636fcd8572c9bcf50240a25b8972198
osu!Akatsuki Developer <support@akatsuki.pw>
2022-09-03 00:06:00 +0000

    create database class

---
+++ app/objects/database.py
@@ -0,0 +1,49 @@
+from typing import Any, Mapping
+from sqlalchemy.sql import ClauseElement
+
+import databases
+
+
+class Database:
+    def __init__(self, read_dsn: str, write_dsn: str) -> None:
+        self.read_database = databases.Database(read_dsn)
+        self.write_database = databases.Database(write_dsn)
+
+    async def connect(self) -> None:
+        await self.read_database.connect()
+        await self.write_database.connect()
+
+    async def disconnect(self) -> None:
+        await self.read_database.disconnect()
+        await self.write_database.disconnect()
+
+    async def fetch_val(
+        self,
+        query: ClauseElement | str,
+        values: dict | None = None,
+        column: Any = 0,
+    ) -> Any:
+        return await self.read_database.fetch_val(query, values, column)
+
+    async def fetch_one(
+        self,
+        query: ClauseElement | str,
+        values: dict | None = None,
+    ) -> Mapping:
+        return (await self.read_database.fetch_one(query, values))._mapping
+
+    async def fetch_all(
+        self,
+        query: ClauseElement | str,
+        values: dict | None = None,
+    ) -> list[Mapping]:
+        return [
+            row._mapping for row in await self.read_database.fetch_all(query, values)
+        ]
+
+    async def execute(
+        self,
+        query: ClauseElement | str,
+        values: dict | None = None,
+    ) -> Any:
+        return await self.write_database.execute(query, values)
