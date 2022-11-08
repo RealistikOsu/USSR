ed0605637d400fae0d4bdb746d48d9dd358fb617
James <tsunyoku@gmail.com>
2022-11-08 16:29:38 +0000

    fix: don't save bancho beatmap to database when fetching map from pp api

--- app/api/pp.py
+++ app/api/pp.py
@@ -39,7 +39,7 @@ async def calculate_pp(
 
     use_common_pp_percentages = acc is None
 
-    beatmap = await app.usecases.beatmap.id_from_api(beatmap_id)
+    beatmap = await app.usecases.beatmap.id_from_api(beatmap_id, save=False)
     if not beatmap:
         return ORJSONResponse(
             content={"message": "Invalid/non-existent beatmap id."},
