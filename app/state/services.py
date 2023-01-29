7e2030ebb40001b0c607eb153cb30fff81c75f1b
RealistikDash <realistikdash@gmail.com>
2023-01-29 18:37:25 +0100

    fix: quote sql passwords in dsn

--- app/state/services.py
+++ app/state/services.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from urllib.parse import quote
+
 import aiohttp
 import aioredis
 import databases
@@ -12,7 +14,7 @@ redis: aioredis.Redis = aioredis.from_url("redis://localhost")
 url = databases.DatabaseURL(
     "mysql+asyncmy://{username}:{password}@{host}:3306/{db}".format(
         username=config.sql_user,
-        password=config.sql_pass,
+        password=quote(config.sql_pass),
         host=config.sql_host,
         db=config.sql_db,
     ),
