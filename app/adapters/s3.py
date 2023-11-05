5036a2aa19fb6a9ee4017bca6bff8c77d3625b3d
cmyui <cmyuiosu@gmail.com>
2023-11-05 10:29:30 -0500

    handle NoSuchKey on s3 download gracefully

--- app/adapters/s3.py
+++ app/adapters/s3.py
@@ -41,6 +41,8 @@ async def download(file_name: str, folder: str) -> bytes | None:
             Bucket=config.AWS_BUCKET_NAME,
             Key=f"{folder}/{file_name}",
         )
+    except services.s3_client.exceptions.NoSuchKey:
+        return None
     except (botocore.exceptions.BotoCoreError, botocore.exceptions.ClientError) as exc:
         logging.error("Failed to download file from S3", exc_info=exc)
         return None
