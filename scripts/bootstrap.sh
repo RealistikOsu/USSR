e75b0549f55208d97ffb4032d76f5639f625546f
Josh Smith <cmyuiosu@gmail.com>
2024-06-16 00:25:12 -0400

    Switch vault to use $APP_ENV

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -8,8 +8,7 @@ fi
 
 if [[ $PULL_SECRETS_FROM_VAULT -eq 1 ]]; then
   echo "Fetching secrets from vault"
-  # TODO: revert to $APP_ENV
-  akatsuki vault get score-service production-k8s -o .env
+  akatsuki vault get score-service $APP_ENV -o .env
   echo "Fetched secrets from vault"
   source .env
   echo "Sourced secrets from vault"
